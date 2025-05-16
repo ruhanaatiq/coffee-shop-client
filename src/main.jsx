b77b6d625d4a7105910cae7c08b89bb43b419430
ruhanaatiq <ruhana.atiq@gmail.com>
2025-05-16 10:28:32 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Home from './components/Home.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import MainLayout from './layouts/MainLayout.jsx';
+import AddCoffee from './components/AddCoffee.jsx'
+import CoffeeDetails from './components/CoffeeDetails.jsx'
+import UpdateCoffee from './components/UpdateCoffee.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+Component: MainLayout,
+children:[
+  {
+    index: true,
+            loader: () => fetch('http://localhost:3000/coffees'),
+
+    Component: Home
+  },
+  {
+    path:'addCoffee', 
+    Component: AddCoffee
+  },
+    {
+        path: 'coffee/:id',
+        Component: CoffeeDetails
+      },
+  {
+    path: 'updateCoffee/:id',
+     loader: ({ params }) => fetch(`http://localhost:3000/coffees/${params.id}`),
+    Component: UpdateCoffee
+  }
+]  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
